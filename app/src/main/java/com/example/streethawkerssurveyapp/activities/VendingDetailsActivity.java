20c137ddaac61298b4ea5287974c9cc6fb5a88ed
webplat9@gmail.com <Webplat7741081323>
2020-07-11 00:46:50 +0530

    to nishad

--- app/src/main/java/com/example/streethawkerssurveyapp/activities/VendingDetailsActivity.java
+++ app/src/main/java/com/example/streethawkerssurveyapp/activities/VendingDetailsActivity.java
@@ -1,6 +1,7 @@
 package com.example.streethawkerssurveyapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import okhttp3.MediaType;
 import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
@@ -8,15 +9,20 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+import android.app.DatePickerDialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextThemeWrapper;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 
 import com.example.streethawkerssurveyapp.R;
@@ -29,6 +35,7 @@ import com.example.streethawkerssurveyapp.services_pack.CustomProgressDialog;
 import com.example.streethawkerssurveyapp.utils.PrefUtils;
 
 import java.io.File;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -54,23 +61,26 @@ public class VendingDetailsActivity extends AppCompatActivity {
     private Spinner mSpinnerChoice;
     private Button mBtnNext;
     private Button mBtnPrevious;
+    RadioGroup RGDocument, RGVendor;
 
     private ProgressDialog progressDialog;
 
     private String
-    URI_NO="",
-    TYPE_OF_VENDING="",
-    VENDING_SITE="",
-    VENDING_FROM="",
-    VENDING_TO="",
-    YRS_OF_VENDING="",
-    ANNUAL_INCOME="",
-    IS_RECOGNIZED_STREET_VENDOR="",
-    TYPE_OF_STRUCTURE="",
-    STARTING_DATE_VENDING="",
-    TEHABZARI_AVAILABLE="",
-    VENDING_AREA_CHOCE="";
+            URI_NO = "",
+            TYPE_OF_VENDING = "",
+            VENDING_SITE = "",
+            VENDING_FROM = "",
+            VENDING_TO = "",
+            YRS_OF_VENDING = "",
+            ANNUAL_INCOME = "",
+            IS_RECOGNIZED_STREET_VENDOR = "",
+            TYPE_OF_STRUCTURE = "",
+            STARTING_DATE_VENDING = "",
+            TEHABZARI_AVAILABLE = "",
+            VENDING_AREA_CHOCE = "";
 
+    private Calendar myCalendar;
+    private int mYear, mMonth, mDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,11 +89,37 @@ public class VendingDetailsActivity extends AppCompatActivity {
 
         bindView();
 
+        myCalendar = Calendar.getInstance();
+        mYear = myCalendar.get(Calendar.YEAR);
+        mMonth = myCalendar.get(Calendar.MONTH);
+        mDay = myCalendar.get(Calendar.DAY_OF_MONTH);
+
+        mImgCalendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(new ContextThemeWrapper(VendingDetailsActivity.this, R.style.DialogTheme),
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+
+                                mEditDob.setText((monthOfYear + 1) + "/" + dayOfMonth + "/" + year);
+
+
+                            }
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+
+            }
+        });
+
         mBtnPrevious.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                startActivity(new Intent(VendingDetailsActivity.this,VendorsFamDetailsActivity.class));
+                startActivity(new Intent(VendingDetailsActivity.this, VendorsFamDetailsActivity.class));
             }
         });
         mBtnNext.setOnClickListener(new View.OnClickListener() {
@@ -91,23 +127,75 @@ public class VendingDetailsActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 if (mLinearOne.getVisibility() == View.VISIBLE) {
-                    if(validate1()) {
+                    if (validate1()) {
                         mLinearOne.setVisibility(View.GONE);
                         mLinearTwo.setVisibility(View.VISIBLE);
                         mLinearThree.setVisibility(View.GONE);
                     }
                 } else if (mLinearTwo.getVisibility() == View.VISIBLE) {
-                    if(validate2()) {
+                    if (validate2()) {
                         mLinearTwo.setVisibility(View.GONE);
                         mLinearOne.setVisibility(View.GONE);
                         mLinearThree.setVisibility(View.VISIBLE);
                     }
-                }
-                else {
+                } else {
 
-                   URI_NO = ApplicationConstant.URI_NO;
+                    URI_NO = ApplicationConstant.URI_NO;
+                    mSpinnerItems.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-                    UpdateSurvey();
+                            TYPE_OF_VENDING = mSpinnerItems.getItemAtPosition(position).toString();
+
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+
+                        }
+                    });
+
+                    VENDING_SITE=mEditVendingSite.getText().toString().trim();
+                    VENDING_FROM=mEditFromTime.getText().toString().trim();
+                    VENDING_TO=mEditToTime.getText().toString().trim();
+                    YRS_OF_VENDING=mEditAge.getText().toString().trim();
+                    ANNUAL_INCOME=mEditAnnualIncome.getText().toString().trim();
+                    ANNUAL_INCOME=mEditAnnualIncome.getText().toString().trim();
+                    IS_RECOGNIZED_STREET_VENDOR = String.valueOf(RGVendor.getCheckedRadioButtonId());
+                    mSpinnerVehical.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                            TYPE_OF_STRUCTURE = mSpinnerVehical.getItemAtPosition(position).toString();
+
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+
+                        }
+                    });
+
+                    STARTING_DATE_VENDING=mEditDob.getText().toString().trim();
+                    TEHABZARI_AVAILABLE= String.valueOf(RGDocument.getCheckedRadioButtonId());
+                    mSpinnerChoice.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                            VENDING_AREA_CHOCE = mSpinnerChoice.getItemAtPosition(position).toString();
+
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+
+                        }
+                    });
+
+
+                    if (validate3()) {
+                        UpdateSurvey();
+                    }
 
 
                 }
@@ -117,11 +205,36 @@ public class VendingDetailsActivity extends AppCompatActivity {
         });
     }
 
+    private boolean validate3() {
+        if (!ApplicationConstant.isNetworkAvailable(VendingDetailsActivity.this)) {
+
+            ApplicationConstant.displayMessageDialog(VendingDetailsActivity.this, "No Internet Connection", "Please enable internet connection first to proceed");
+
+            return false;
+        } else if (mSpinnerChoice.getSelectedItem().toString().isEmpty()) {
+            mSpinnerChoice.requestFocus();
+            return false;
+        }
+        return true;
+    }
+
     private boolean validate2() {
         if (!ApplicationConstant.isNetworkAvailable(VendingDetailsActivity.this)) {
 
             ApplicationConstant.displayMessageDialog(VendingDetailsActivity.this, "No Internet Connection", "Please enable internet connection first to proceed");
 
+            return false;
+        } else if (mEditAnnualIncome.getText().toString().trim().isEmpty()) {
+            mEditAnnualIncome.setError("Enter Vending Site");
+            mEditAnnualIncome.requestFocus();
+            return false;
+        } else if (mSpinnerVehical.getSelectedItem().toString().isEmpty()) {
+            mEditAnnualIncome.setError("Select Vehicals");
+            mSpinnerVehical.requestFocus();
+            return false;
+        } else if (mEditDob.getText().toString().trim().isEmpty()) {
+            mEditDob.setError("Enter Date");
+            mEditDob.requestFocus();
             return false;
         }
         return true;
@@ -134,23 +247,23 @@ public class VendingDetailsActivity extends AppCompatActivity {
             ApplicationConstant.displayMessageDialog(VendingDetailsActivity.this, "No Internet Connection", "Please enable internet connection first to proceed");
 
             return false;
-        }else if(mSpinnerItems.getSelectedItem().toString().isEmpty()){
+        } else if (mSpinnerItems.getSelectedItem().toString().isEmpty()) {
             mEditVendingSite.setError("Select Items");
             mSpinnerItems.requestFocus();
             return false;
-        }else if(mEditVendingSite.getText().toString().trim().isEmpty()){
+        } else if (mEditVendingSite.getText().toString().trim().isEmpty()) {
             mEditVendingSite.setError("Enter Vending Site");
             mEditVendingSite.requestFocus();
             return false;
-        }else if(mEditFromTime.getText().toString().trim().isEmpty()){
+        } else if (mEditFromTime.getText().toString().trim().isEmpty()) {
             mEditFromTime.setError("Enter From Time");
             mEditFromTime.requestFocus();
             return false;
-        }else if(mEditToTime.getText().toString().trim().isEmpty()){
+        } else if (mEditToTime.getText().toString().trim().isEmpty()) {
             mEditToTime.setError("Enter To Time");
             mEditToTime.requestFocus();
             return false;
-        }else if(mEditAge.getText().toString().trim().isEmpty()){
+        } else if (mEditAge.getText().toString().trim().isEmpty()) {
             mEditAge.setError("Enter Age");
             mEditAge.requestFocus();
             return false;
@@ -163,6 +276,8 @@ public class VendingDetailsActivity extends AppCompatActivity {
 
         mLinearMain = (LinearLayout) findViewById(R.id.LinearMain);
         mLinearOne = (LinearLayout) findViewById(R.id.LinearOne);
+        RGDocument = (RadioGroup) findViewById(R.id.RGDocument);
+        RGVendor = (RadioGroup) findViewById(R.id.RGVendor);
         mSpinnerItems = (Spinner) findViewById(R.id.SpinnerItems);
         mEditVendingSite = (EditText) findViewById(R.id.EditVendingSite);
         mEditFromTime = (EditText) findViewById(R.id.EditFromTime);
@@ -170,14 +285,14 @@ public class VendingDetailsActivity extends AppCompatActivity {
         mEditAge = (EditText) findViewById(R.id.EditAge);
         mLinearTwo = (LinearLayout) findViewById(R.id.LinearTwo);
         mEditAnnualIncome = (EditText) findViewById(R.id.EditAnnualIncome);
-        mRadioY = (RadioButton) findViewById(R.id.RadioY);
-        mRadioN = (RadioButton) findViewById(R.id.RadioN);
+        mRadioY = (RadioButton) findViewById(Integer.parseInt(IS_RECOGNIZED_STREET_VENDOR));
+        mRadioN = (RadioButton) findViewById(Integer.parseInt(IS_RECOGNIZED_STREET_VENDOR));
         mSpinnerVehical = (Spinner) findViewById(R.id.SpinnerVehical);
         mEditDob = (EditText) findViewById(R.id.EditDob);
         mImgCalendar = (ImageView) findViewById(R.id.ImgCalendar);
         mLinearThree = (LinearLayout) findViewById(R.id.LinearThree);
-        mRadioDY = (RadioButton) findViewById(R.id.RadioDY);
-        mRadioDN = (RadioButton) findViewById(R.id.RadioDN);
+        mRadioDY = (RadioButton) findViewById(Integer.parseInt(TEHABZARI_AVAILABLE));
+        mRadioDN = (RadioButton) findViewById(Integer.parseInt(TEHABZARI_AVAILABLE));
         mSpinnerChoice = (Spinner) findViewById(R.id.SpinnerChoice);
         mBtnNext = (Button) findViewById(R.id.BtnNext);
         mBtnPrevious = (Button) findViewById(R.id.BtnPrevious);
@@ -192,13 +307,13 @@ public class VendingDetailsActivity extends AppCompatActivity {
         String username = PrefUtils.getFromPrefs(VendingDetailsActivity.this, ApplicationConstant.USERDETAILS.API_KEY, "");
 
         Map<String, String> headers = new HashMap<>();
-        headers.put("Authorization", "Bearer "+PrefUtils.getFromPrefs(VendingDetailsActivity.this,ApplicationConstant.USERDETAILS.API_KEY,""));
+        headers.put("Authorization", "Bearer " + PrefUtils.getFromPrefs(VendingDetailsActivity.this, ApplicationConstant.USERDETAILS.API_KEY, ""));
 
         ApiInterface apiservice = ApiService.getApiClient().create(ApiInterface.class);
         Call<UpdateSurveyResponse> call = apiservice.getUpdateSurvey(headers,
-                URI_NO,TYPE_OF_VENDING,VENDING_SITE,VENDING_FROM,VENDING_TO,YRS_OF_VENDING,
-                ANNUAL_INCOME,IS_RECOGNIZED_STREET_VENDOR,TYPE_OF_STRUCTURE,STARTING_DATE_VENDING,
-                TEHABZARI_AVAILABLE,VENDING_AREA_CHOCE
+                URI_NO, TYPE_OF_VENDING, VENDING_SITE, VENDING_FROM, VENDING_TO, YRS_OF_VENDING,
+                ANNUAL_INCOME, IS_RECOGNIZED_STREET_VENDOR, TYPE_OF_STRUCTURE, STARTING_DATE_VENDING,
+                TEHABZARI_AVAILABLE, VENDING_AREA_CHOCE
 
         );
 
@@ -213,11 +328,10 @@ public class VendingDetailsActivity extends AppCompatActivity {
 
                     if (response.body().isStatus()) {
 
-                        ApplicationConstant.DisplayMessageDialog(VendingDetailsActivity.this,"",
+                        ApplicationConstant.DisplayMessageDialog(VendingDetailsActivity.this, "",
                                 "Vending Details saved successfully");
 
 //                        startActivity(new Intent(VendingDetailsActivity.this, DocumentScanActivity.class));
-
 
 
                     } else {
@@ -227,13 +341,13 @@ public class VendingDetailsActivity extends AppCompatActivity {
                                 String.valueOf(response.body().isStatus()));
                     }
 
-                }else {
+                } else {
 
                     try {
                         ApplicationConstant.displayMessageDialog(VendingDetailsActivity.this,
                                 "Response",
                                 response.errorBody().string());
-                    }catch (Exception e){
+                    } catch (Exception e) {
                         e.printStackTrace();
                     }
 
@@ -250,7 +364,6 @@ public class VendingDetailsActivity extends AppCompatActivity {
             }
         });
     }
-
 
 
 }
